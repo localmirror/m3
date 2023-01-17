9cd0785d6d6393f065147ffd8e2624914b14f825
localmirror <121659332+localmirror@users.noreply.github.com>
2023-01-17 21:15:38 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/LazyDeveloperr/LazyPrincessV2 /LazyPrincessV2
+  git clone https://github.com/localmirror/m3
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LazyPrincessV2
+  git clone $UPSTREAM_REPO /m3
 fi
-cd /LazyPrincessV2
+cd /m3
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
